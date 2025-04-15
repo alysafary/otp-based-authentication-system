065e97e58f0d56823a308e84a4f3bc9221f1b010
ali <alysfryly075@gmail.com>
2025-04-15 17:10:06 +0330

    Update URLs

--- users/urls.py
+++ users/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from users.views import LoginRequestView, PasswordAuthenticationView, OTPVerifyView, SignupCompleteView
 
 urlpatterns = [
-    path('login/', LoginRequestView.as_view(), name='login_request'),
-    path('authenticate/', PasswordAuthenticationView.as_view(), name='password_authenticate'),
-    path('verify-otp/', OTPVerifyView.as_view(), name='otp_verify'),
-    path('signup-complete/', SignupCompleteView.as_view(), name='signup_complete'),
+    path("login/request/", LoginRequestView.as_view(), name='login_request'),
+    path("login/password/", PasswordAuthenticationView.as_view(), name='password_authenticate'),
+    path("login/verify/", OTPVerifyView.as_view(), name='otp_verify'),
+    path("signup/complete/", SignupCompleteView.as_view(), name='signup_complete'),
 ]
